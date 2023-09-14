0b1c432669065a445dce3babfd36fdb3efd6f877
Rebecca Tholden <rebecca.tholden@volvo.com>
2023-09-14 09:35:39 +0200

    README.py

---
+++ README.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  6 12:01:41 2023
+
+@author: A304088
+"""
+# Explore-US-Bikeshare-Data
+Udacity Data Analyst Degree - Project II
+
+# I've used below link to learn more about while loops
+https://www.programiz.com/python-programming/while-loop
+
+# I've used below link to search and learn from other scripts
+https://github.com/
+
+# I've used below link to understand more about error messages that I received while creating the script
+https://www.learndatasci.com/solutions/python-keyerror/
+
+# I've used the book "Learn Python the Hard Way 3" by Zed Shaw
